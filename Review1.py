c867db93d7c90a6ad251687d1ddf596bdd92bbed
rohk30 <134207791+rohk30@users.noreply.github.com>
2024-03-20 23:37:40 +0530

    Class name voice-over images

--- Review1.py
+++ Review1.py
@@ -1,60 +1,85 @@
 from ultralytics import YOLO
 import cv2
 import pyttsx3
+import random
+import threading
 
-model_path = "best.pt"  
+
+model_path = "best.pt"
+
+
 
 model = YOLO(model_path)
-
-def get_class(name: str):
-        return list(model.names.values()).index(name)
-
-def get_mask(name: str, results):
-        return (results[0].boxes.cls == get_class(name))       
-
-cap = cv2.VideoCapture(0)
 
 engine = pyttsx3.init()
 engine.setProperty('rate', 150)
 
-engine.say("Hello guys, Welcome to the SignSentry.")
+engine.say("Welcome to SignSentry")
 engine.runAndWait()
 
-while True:
-    ret, frame = cap.read()
+# def get_class_name(results):
 
-    results = model.predict(frame)
+#   class_id = results[0].boxes.cls[0]
+#   class_name = model.names[class_id]
+#   return class_name
 
-    #mask_phone = get_mask('person', results)
+
+def get_class(name: str):
+    return list(model.names.values()).index(name)
+
+def get_mask(name: str, results):
+    return (results[0].boxes.cls == get_class(name))       
+
+def process_image():
+    randNo = random.randint(0, 12)
+    path_file = "RS" + str(randNo) + ".jpg"
+    img = cv2.imread(path_file)
+
+
+    results = model.predict(img)
     bboxes = results[0].boxes.xyxy.tolist()
-    #bboxes = results[0].xyxy.tolist()
+
+    
 
 
-    if len(bboxes) > 0:
-        print("Sign detected")
-        for bbox in bboxes:
-                start_point = (int(bbox[0]), int(bbox[1]))
-                end_point = (int(bbox[2]), int(bbox[3]))
-                cv2.rectangle(frame, start_point, end_point, (255, 0, 0), 2)
+    for bbox in bboxes:
+        start_point = (int(bbox[0]), int(bbox[1]))
+        end_point = (int(bbox[2]), int(bbox[3]))
+        cv2.rectangle(img, start_point, end_point, (255, 0, 0), 2)
 
-                cv2.putText(frame, str("sign"), (start_point[0], start_point[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+        cv2.putText(img, "sign", (start_point[0], start_point[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
 
-                class_id = results[0].boxes.cls[0]
-                id_int = class_id.item()
-                print(results[0].names[(int)(id_int)])
+        class_id = results[0].boxes.cls[0]
+        print(type(class_id))
+        print(class_id)
+        id_int = class_id.item()
+        print(type(id_int))
+        print(results[0].names[(int)(id_int)])
+        print(results)
+        #class_name = model.names[class_id]
 
-                class_detect = results[0].names[(int)(id_int)]
-                engine.say(class_detect)
+    # class_name = get_class_name(results)
+        #print(f"Class name: {class_name}")
 
-                #engine.say("Sign detected")
-                engine.runAndWait()
+        class_detect = results[0].names[(int)(id_int)]
+        engine.say(class_detect)
+        engine.runAndWait()
+        engine.stop()
 
-    cv2.imshow("Bottle Detection", frame)
+
+    cv2.imshow("Bottle Detection", img)
 
 
+    threading.Timer(3, process_image).start()
+
+
+process_image()
+
+
+while True:
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 
-cap.release()
 cv2.destroyAllWindows()
+
